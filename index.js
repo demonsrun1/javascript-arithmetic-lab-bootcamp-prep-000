fcd57bb141e95303bf8d46e33d3d1d75ba9f27ef
Alec Staszewski <astaszew@gmail.com>
2018-07-09 21:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function subtract(a, b){
 function multiply(a, b){
   return a * b;
 }
+
+function divide(a,b){
+  return a /b;
+}
+function inc(n){
+  return ++n;
+}
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+  return parseInt(n);
+}
